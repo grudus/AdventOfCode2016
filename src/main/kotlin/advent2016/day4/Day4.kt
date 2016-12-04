be081a7c980223c82d0a402deb7a7730d0da2e1a
grudus <grudus@protonmail.com>
2016-12-04 12:35:43 +0100

    cleaning

--- src/main/kotlin/advent2016/day4/Day4.kt
+++ src/main/kotlin/advent2016/day4/Day4.kt
@@ -1,57 +1,57 @@
 package advent2016.day4
 
 import advent2016.Day
+import java.util.regex.Pattern
 import kotlin.comparisons.compareByDescending
 import kotlin.comparisons.thenBy
 
 class Day4(input: String? = null) : Day(input) {
 
-    val inputs: List<Tuple> = generateInput()
-    val secondStarInputs: List<Pair<String, Int>> = generateSecondStarInput()
-
-    private fun  generateInput(): List<Tuple> {
+    private fun firstStarInput(): List<Tuple> {
         return super.inputString.split(Regex("\\n"))
                 .map { string ->
-                    val indexOfLastDash = string.lastIndexOf('-')
-                    val indexOfBrace = string.indexOf('[')
-                    val pair = string.substring(0, indexOfLastDash).replace("-", "")
+                    val charOccurrenceMap = string.replace("-", "").find(Regex("[a-z]+"))
                             .groupBy { it }.map { Pair(it.key, it.value.size) }.toMap()
 
-                    Tuple(pair, string.substring(indexOfLastDash+1, indexOfBrace).toInt(), string.substring(indexOfBrace+1, string.length-1))
+                    Tuple(charOccurrenceMap, string.find(Regex("\\d+")).toInt(), string.find(Regex("(?<=\\[).+?(?=\\])")))
                 }
     }
 
-    private fun  generateSecondStarInput(): List<Pair<String, Int>> {
+    private fun secondStarInput(): List<Pair<String, Int>> {
         return super.inputString.split(Regex("\\n"))
-                .map {
-                    val indexOfLastDash = it.lastIndexOf('-')
-                    val indexOfBrace = it.indexOf('[')
-                    Pair(it.substring(0, indexOfLastDash), it.substring(indexOfLastDash+1, indexOfBrace).toInt())
-                }
+                .map { Pair(it.find(Regex("[a-z-]+")), it.find(Regex("\\d+")).toInt()) }
     }
 
 
-    private fun isValid(tuple: Tuple) : Boolean {
-        val sorted = tuple.map.toList().sortedWith(compareByDescending<Pair<Char, Int>> { it.second }.thenBy { it.first } )
+    private fun isValid(tuple: Tuple): Boolean {
+        val sorted = tuple.charOccurrence.toList().sortedWith(compareByDescending<Pair<Char, Int>> { it.second }.thenBy { it.first })
         return sorted.subList(0, tuple.checkSum.length).map { it.first.toString() }.joinToString("") == tuple.checkSum
     }
 
     override fun firstStar(): String {
-        return inputs.filter { it -> isValid(it)}.sumBy { it.id }.toString()
+        return firstStarInput().filter { it -> isValid(it) }.sumBy { it.id }.toString()
     }
 
     override fun secondStar(): String {
-        val stringIdPairs = secondStarInputs.map {
-            pair -> Pair(cipher(pair.first, pair.second), pair.second) }
+        val stringIdPairs = secondStarInput().map {
+            pair ->
+            Pair(cipher(pair.first, pair.second), pair.second)
+        }
         return stringIdPairs.find { it.first.startsWith("northpole") }?.second.toString()
     }
 
-    fun cipher(string: String, shift: Int) : String = string.map { cipher(it, shift) }.joinToString("").trim()
+    fun cipher(string: String, shift: Int): String = string.map { cipher(it, shift) }.joinToString("").trim()
 
-    fun cipher(char: Char, shift: Int) : Char {
+    fun cipher(char: Char, shift: Int): Char {
         if (char == '-' || !Character.isAlphabetic(char.toInt())) return ' '
         val realShift = shift % 26
-        return if (char + realShift > 'z') char - (26-realShift) else char + realShift
+        return if (char + realShift > 'z') char - (26 - realShift) else char + realShift
     }
 }
-data class Tuple(val map: Map<Char, Int>, val id: Int, val checkSum: String)
+
+data class Tuple(val charOccurrence: Map<Char, Int>, val id: Int, val checkSum: String)
+
+fun String.find(regex: Regex): String {
+    val m = Pattern.compile(regex.pattern).matcher(this)
+    return if (m.find()) m.group() else ""
+}
